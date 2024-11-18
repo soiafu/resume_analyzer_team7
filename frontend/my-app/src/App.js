fc39226e192e991a5a6da22bedbd3f9373124c15
ercac <140350976+ercac@users.noreply.github.com>
2024-11-18 17:38:19 -0500

    dashboard implementation

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -2,20 +2,38 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Login, Register } from './login'; 
+import Dashboard from './dashboard/Dashboard';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 function App() {
+  const fitScore = 60;
+  const matchedSkills = ['Python', 'Java', 'Javascript', 'React.js'];
+  const suggestions = [
+    'Suggestion 1',
+    'Suggestion 2',
+    'Suggestion 3'
+  ];
   return (
     <Router>
       <div className="App">
+      
         <Routes>
           <Route path="/"
             element={
               <div >
                 <div><Login /></div>
                 <div><Register /></div>
+                <Dashboard
+                  fitScore={fitScore}
+                  matchedSkills={matchedSkills}
+                  suggestions={suggestions}
+                />
               </div>
+              
             }
           />
+          
         </Routes>
       </div>
     </Router>
